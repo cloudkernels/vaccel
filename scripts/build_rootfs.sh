23be13395d7324c442cd810ba30d6a28ed3fb8ac
Babis Chalios <mail@bchalios.io>
2021-08-13 15:34:57 +0200

    Add a workflow that builds a Firecracker rootfs
    Add a workflow that builds a Firecracker root file system every time a
    change in the corresponding Dockerfile occurs and uploads it to s3.
    
    This will help us in the future to speed-up e2e testing, since we will
    not need to build the rootfs every time. Instead we will just need to
    install the vAccel binaries in the rootfs.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- scripts/build_rootfs.sh
+++ scripts/build_rootfs.sh
@@ -25,29 +25,32 @@ print_help() {
 	echo "Usage: build_rootfs.sh [<args>]"
 	echo ""
 	echo "Arguments:"
+	echo "    --help|-h          Print this message and exit"
 	echo "    --build_dir        Directory to use for building (default: 'build')"
 	echo "    --install_prefix   Directory to install library (default: 'output')"
 	echo "    --base_image       Base image to use (default: ubuntu:latest)"
 	echo "    --dockerfiles_path Path to find dockerfiles for base images (default: dockerfiles)"
 	echo ""
+	echo "Available sub-commands"
+	echo ""
+	echo "    build_base_rootfs"
+	echo "        Build the base root file system (without vAccel binaries installed)"
+	echo ""
+	echo "    install_vaccel"
+	echo "        Install vAccel binaries inside root file system"
+	echo ""
+	echo "    all"
+	echo "        Build the base rootfs and then install the vAccel binaries in it"
+	echo ""
+	echo "    help"
+	echo "        Print this message"
+	echo ""
 }
 
-fetch_images() {
-	svn export https://github.com/dusty-nv/jetson-inference/trunk/data/networks networks
-}
 
-prepare_env() {
-	if [ -d ${BUILD_DIR}/rootfs ]; then
-		rm -rf ${BUILD_DIR}/rootfs/*
-	else
-		mkdir -p ${BUILD_DIR}/rootfs
-	fi
-
-	pushd ${BUILD_DIR}/rootfs > /dev/null
-
-	# Bring other components
-	cp -r ${INSTALL_PREFIX}/* .
-	ok_or_die "Could not find components"
+build_base_rootfs() {
+	mkdir -p ${BUILD_DIR}/rootfs
+	cd ${BUILD_DIR}/rootfs
 
 	# Fetch Dockerfile on which we'll base the image
 	local image=$(echo "${BASE_IMAGE}" | awk -F ":" '{print $1}')
@@ -60,20 +63,6 @@ prepare_env() {
 
 	cp ${DOCKERFILES_PATH}/${image}/${tag}/Dockerfile .
 	ok_or_die "Could not find Dockerfile for ${BASE_IMAGE}"
-
-	# Fetch imagenet
-	mkdir -p imagenet
-	pushd imagenet > /dev/null
-
-	fetch_images
-	ok_or_die "Could not fetch imagenet images"
-
-	popd > /dev/null
-	popd > /dev/null
-}
-
-build() {
-	cd ${BUILD_DIR}/rootfs
 
 	# Create root filesystem
 	DOCKER_BUILDKIT=1 docker build \
@@ -103,16 +92,56 @@ build() {
 	ok_or_die "Could not populate rootfs"
 
 	sudo umount $mnt
-	ok_or_die "Could unmount rootfs"
+	ok_or_die "Could not unmount rootfs"
 
 	sudo sync
 	sudo rmdir $mnt
 
 	cp rootfs.img ${INSTALL_PREFIX}/share/
-	cp -r imagenet/networks ${INSTALL_PREFIX}/share/
+}
+
+install_vaccel() {
+	mkdir -p ${BUILD_DIR}/rootfs
+	cd ${BUILD_DIR}/rootfs
+
+	if [ ! -f $INSTALL_PREFIX/share/rootfs.img ]; then
+		die "Base root file system is not built"
+	fi
+
+	mnt="$(mktemp -d)"
+	sudo mount $INSTALL_PREFIX/share/rootfs.img $mnt
+	ok_or_die "Could not mount rootfs"
+
+	if [[ ! -d $mnt/opt/vaccel ]] ; then
+		die "Base rootfs is not built properly"
+	fi
+
+	cp -r $INSTALL_PREFIX/{bin,lib,include,share} $mnt/opt/vaccel/
+	ok_or_die "Could not install vAccel in root file system"
+
+	# Setup VirtIO module (hardcoded kernel version)
+	mkdir -p $mnt/lib/modules/4.20.0
+	cp $INSTALL_PREFIX/share/virtio_accel.ko $mnt/lib/modules/4.20.0
+	touch $mnt/lib/modules/4.20.0/modules.order
+	touch $mnt/lib/modules/4.20.0/modules.builtin
+	echo "virtio_accel" >> $mnt/etc/modules
+	sudo chroot $mnt /sbin/depmod 4.20.0
+
+	sudo sync
+	sudo umount $mnt
+	ok_or_die "Could not unmount root file system"
+}
+
+all() {
+	build_base_rootfs
+	install_vaccel
 }
 
 main() {
+	if [ $# == 0 ]; then
+		die "Insufficient script arguments. Use \`$0 --help\'."
+	fi
+
 	while [ $# -gt 0 ]; do
 		case "$1" in
 			-h|--help)          { print_help; exit 1;         };;
@@ -120,18 +149,21 @@ main() {
 			--install_prefix)   { INSTALL_PREFIX=$2; shift;   };;
 			--base_image)       { BASE_IMAGE=$2; shift;       };;
 			--dockerfiles_path) { DOCKERFILES_PATH=$2; shift; };;
-			*)
+			*-)
 				die "Unknown argument: \"$1\". Please use \`$0 --help\`."
+				;;
+			*)
+				break;
 				;;
 		esac
 		shift
 	done
 
-	# Prepare build environment
-	prepare_env
+	# Make sure that $1 is a valid command
+	declare -f "$1" > /dev/null
+	ok_or_die "Unknown command: '$1'. Please use \`$0 --help\`."
 
-	# and build
-	build
+	$1 "$@"
 }
 
 main "$@"
